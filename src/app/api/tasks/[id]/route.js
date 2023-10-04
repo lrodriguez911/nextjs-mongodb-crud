84e320ade9b381fc6545519a542aa331abd2c851
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-10-03 23:25:38 -0300

    add functions for the crud

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,8 +1,25 @@
 import { NextResponse } from "next/server";
 
-export function GET(request, {params}) {
-    const id = params.id;
-    return NextResponse.json({
-        message: `Hello World ${id}`,
-    }).status(200)
+export function GET(request, { params }) {
+  const id = params.id;
+  return NextResponse.json({
+    message: `Hello World ${id}`,
+  }).status(200);
+}
+
+export function DELETE(request, { params }) {
+  const id = params.id;
+  return NextResponse.json({
+    message: `Deleting task ${id}`,
+  });
+}
+
+export function PUT(request, { params }) {
+  const {id, name, description } = params;
+  const task = find(id);
+    task.name = name;
+    task.description = description;
+  return NextResponse.json({
+    message: `Updating task ${id}`,
+  });
 }
