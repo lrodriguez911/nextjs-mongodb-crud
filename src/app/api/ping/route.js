f8b7f98b2451d76be199778ffec982cc26bced44
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-10-05 23:31:04 -0300

    add config whit mongoose

--- src/app/api/ping/route.js
+++ src/app/api/ping/route.js
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "@/utils/mongoose";
+
 
 export function GET() {
     return NextResponse.json({
--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,11 +1,21 @@
 import {connect, connection} from 'mongoose';
 
+const conn = {
+    isConnected: false
+}
+
 export async function connectDB() {
+    if (conn.isConnected) return;
+    
     const db = await connect('mongodb://localhost/nextmongocrud');
     console.log(db.connection.name);
-   
+   conn.isConnected = db.connections[0].readyState;
 }
 
 connection.on('connected', () => {
     console.log('mongoose is connected');
+})
+
+connection.on('error', (err) => {
+    console.log('mongoose connection error', err);
 })
